00bbd0fb11ec1ddf045cec3b3fddad78d4645561
Kirill Sandow <amphyxs@gmail.com>
2024-11-14 10:14:37 +0300

    hz(app): some stuff from praktika

--- src/main/java/com/example/prac/PracApplication.java
+++ src/main/java/com/example/prac/PracApplication.java
@@ -2,11 +2,14 @@ package com.example.prac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.prac.config.A;
 
 @SpringBootApplication
 public class PracApplication {
 
     public static void main(String[] args) {
+        A a = new A();
+        a.test();
         SpringApplication.run(PracApplication.class, args);
     }
 
---
+++ src/main/java/com/example/prac/config/A.java
@@ -0,0 +1,7 @@
+package com.example.prac.config;
+
+public class A {
+    public void test() {
+        System.out.println("test");
+    }
+}
